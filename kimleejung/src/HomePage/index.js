3f70ae3f047839b2a94820fd6a6c719ba3da21af
odnac <cancode777@gmail.com>
2022-09-20 14:00:36 +0900

    Hompage.Header remove

--- kimleejung/src/HomePage/index.js
+++ kimleejung/src/HomePage/index.js
@@ -1,11 +1,9 @@
-import Header from "./src/Header";
 import Logo from "./src/Logo";
 import Search from "./src/Search";
 
 const index = () => {
   return (
     <div>
-      <Header />
       <Logo />
       <Search />
     </div>
