f9628a3ea39f4900b4b10fddb8e4238949fc1bcc
hun <hunea6686@naver.com>
2022-09-21 17:50:20 +0900

    Detail 수정

--- src/DetailPage/src/Header.js
+++ src/DetailPage/src/Header.js
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation(); // 파라미터 취득
@@ -15,72 +15,175 @@ const Header = () => {
   const onKakao = (e) => {
     e.preventDefault();
     window.location.replace(KAKAO_AUTH_URL);
-  }    
+  };
 
   return (
     <div>
-      <nav className='navbar navbar-light bg-light static-top'>
-        <div className='container'>
-          <a className='navbar-brand' href='/'>
-            {' '}
-            Kimleejung{' '}
-          </a>
-          <a className='navbar-brand' href='/detail'>
+      {/* nav 바 */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light static-top">
+        {/* nav 바 내용 */}
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <a className="navbar-brand" href="/">
+                Kimleejung
+              </a>
+            </div>
+
+            {/* 992 미만일 때 버튼 */}
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {/* 992 미만일 때 묶어주는 역할 */}
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              {/* 검색 한 기업 명 나오는거 
+          <a className="navbar-brand" href="/detail">
             {companyName}
-          </a>
-          <a
-            className='nav-link text-secondary'
-            href='#login'
-            data-bs-toggle='modal'
-            data-bs-target='#staticBackdrop'
-          >
-            login
-          </a>
+          </a> */}
 
-          <div
-            className='modal fade'
-            id='staticBackdrop'
-            data-bs-backdrop='static'
-            data-bs-keyboard='false'
-            tabindex='-1'
-            aria-labelledby='staticBackdropLabel'
-            aria-hidden='true'
-          >
-            <div className='modal-dialog'>
-              <div className='modal-content'>
-                <div className='modal-header'>
-                  <h5 className='modal-title' id='staticBackdropLabel'>
-                    login
-                  </h5>
-                  <button
-                    type='button'
-                    className='btn-close'
-                    data-bs-dismiss='modal'
-                    aria-label='Close'
-                  ></button>
-                </div>
-
-                <div className='modal-body'>
-
-                  <h6 className='modal-sub text-primary'>
-                    {' '}
-                    카카오로 로그인이 가능합니다.{' '}
-                  </h6>
-                  <span class="logo" onClick={onKakao}>
-                    <img src="/img/kakao_login.png" alt="카카오톡"/>
-                  </span>
-                </div>
-                
-                <div className='modal-footer'>
-                  <button
-                    type='button'
-                    className='btn btn-secondary'
-                    data-bs-dismiss='modal'
+              {/* 코스피 */}
+              <div className="col-3">
+                <div className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDarkDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
                   >
-                    Close
-                  </button>
+                    코스피
+                  </a>
+                  <ul
+                    className="dropdown-menu dropdown-menu-dark"
+                    aria-labelledby="navbarDarkDropdownMenuLink"
+                  >
+                    {/* 코스피 내용 */}
+                    <li>
+                      <a className="dropdown-item">기업1</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">기업2</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">기업3</a>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* 코스닥 */}
+                <div className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDarkDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    코스닥
+                  </a>
+                  <ul
+                    className="dropdown-menu dropdown-menu-dark"
+                    aria-labelledby="navbarDarkDropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item">기업1</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">기업2</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">기업3</a>
+                    </li>
+                  </ul>
                 </div>
               </div>
+
+              {/* 로그인 */}
+              <div className="col-3">
+                <a
+                  className="nav-link text-secondary"
+                  href="#login"
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop"
+                >
+                  login
+                </a>
+
+                <div
+                  className="modal fade"
+                  id="staticBackdrop"
+                  data-bs-backdrop="static"
+                  data-bs-keyboard="false"
+                  tabindex="-1"
+                  aria-labelledby="staticBackdropLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      {/* 로그인 모달 상단 */}
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="staticBackdropLabel">
+                          login
+                        </h5>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+
+                      {/* 로그인 모달 내용 */}
+                      <div className="modal-body">
+                        <h6 className="modal-sub text-primary">
+                          {" "}
+                          카카오로 로그인이 가능합니다.{" "}
+                        </h6>
+                        <span className="logo" onClick={onKakao}>
+                          <img src="/img/kakao_login.png" alt="카카오톡" />
+                        </span>
+                      </div>
+
+                      {/* 로그인 모달 하단 */}
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* 기업 검색 */}
+            <div className="col-3">
+              <form class="d-flex">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="기업명을 입력하세요."
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
             </div>
           </div>
         </div>
