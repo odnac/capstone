75af0d511f8f36b15b6a9c283e727363379e284a
odnac <cancode777@gmail.com>
2022-09-27 15:51:17 +0900

    주가 선 차트 - 기간 버튼 추가

--- src/DetailPage/src/Body/src/StockPrice.js
+++ src/DetailPage/src/Body/src/StockPrice.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -21,44 +22,100 @@ ChartJS.register(
   PointElement,
   Filler
 );
-// 주가
+/*
+  날짜랑 데이터 바꿔야 함
+*/
 const StockPriceChart = () => {
-  const labels = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "Jun",
-    "July",
-    "Aug",
-    "Sep",
-  ];
-  const data = {
-    labels: labels,
+  const [data, setData] = useState({
+    labels: [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "July",
+      "Aug",
+      "Sep",
+    ],
     datasets: [
       {
         label: "검색한 기업",
         data: [65, 59, 80, 81, 56, 55, 40, 10, 150],
         fill: true,
         borderColor: "rgb(75, 192, 192)",
-        tension: 0.5,
-      },
-      {
-        label: "비교 기업",
-        data: [15, 37, 75, 55, 90, 90, 90, 150, 60],
-        fill: true,
-        borderColor: "rgb(0,15,192)",
-        tension: 0.5,
+        tension: 0.3,
       },
     ],
+  });
+
+  const onChange3M = (e) => {
+    setData({
+      labels: ["Jan", "Feb", "March"],
+      datasets: [
+        {
+          label: "3개월 주가",
+          data: [65, 59, 80],
+          fill: true,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.3,
+        },
+      ],
+    });
+  };
+
+  const onChange6M = (e) => {
+    setData({
+      labels: ["Jan", "Feb", "March", "April", "May", "Jun"],
+      datasets: [
+        {
+          label: "6개월 주가",
+          data: [65, 59, 80, 81, 56, 55],
+          fill: true,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.3,
+        },
+      ],
+    });
+  };
+
+  const onChange1Y = (e) => {
+    setData({
+      labels: [
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "Jun",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      datasets: [
+        {
+          label: "1년 주가",
+          data: [65, 59, 80, 81, 56, 55, 90, 100, 109, 110, 111, 112],
+          fill: true,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.3,
+        },
+      ],
+    });
   };
 
   return (
     <div>
       <h1>주가 선 차트</h1>
       <Line data={data} />
+      <button onClick={onChange3M}>3개월</button>
+      <button onClick={onChange6M}>6개월</button>
+      <button onClick={onChange1Y}>1년</button>
     </div>
   );
 };
+
 export default StockPriceChart;
