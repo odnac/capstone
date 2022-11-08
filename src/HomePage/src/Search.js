ab4dc7912e6de40c3c60d3f7104d8220d89d6d99
unknown <hanmajsh1@gmail.com>
2022-11-08 22:40:42 +0900

    검색리스트

--- src/HomePage/src/Search.js
+++ src/HomePage/src/Search.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
-
+import JSONDATA from '../../assets/DATA.json'
 import { useNavigate } from "react-router-dom";
 import http from "../../api/http";
 
 const Search = () => {
   const navigate = useNavigate(); // 페이지 이동 시 파라미터 전달
   const [userCompany, setUserCompany] = useState(""); // 검색창에 입력한 기업명 또는 기업번호
-
-  /* 사용자가 검색창에 입력한 기업명 기업번호 userCompany로 받음 */
-  const getUserInputCompany = (e) => {
-    setUserCompany(e.target.value);
-  };
 
   /* 입력한 기업이 더미데이터에 있는지 확인 */
   const onCheckData = (e) => {
@@ -37,16 +32,34 @@ const Search = () => {
                 className="form-control border border-5 border-primary rounded-pill"
                 placeholder="기업명을 입력하세요."
                 value={userCompany}
-                onChange={getUserInputCompany}
+                onChange={(e) => setUserCompany(e.target.value)}
               />
+
               {/* 검색버튼 */}
               <div className="input-group-btn">
                 <button className="btn btn-default" type="submit">
                   <img src="/img/search.svg" />
                 </button>
               </div>
+              
             </div>
           </form>
+          {/*검색 기능*/}
+          <div  className="search_list bg-white">
+            {JSONDATA.filter((val) => {
+              if (userCompany === ""){
+                return null
+              } else if (val.company.toLowerCase().includes(userCompany.toLowerCase())){
+                return val
+              }
+            }).map((val,key)=> {
+              return (
+              <div className="user" key={key} onClick={()=>setUserCompany(val.company)}> 
+                <p> {val.company} </p>
+              </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
